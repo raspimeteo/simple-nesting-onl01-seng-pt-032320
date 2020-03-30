6c5c9a3d2d20aaaebfc6dfd041d6b8f8889cb1f7
John Zonneveld <jjczon@gmail.com>
2020-03-30 23:30:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,7 @@ programmer_hash[:yukihiro_matsumoto]={}
 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 programmer_hash[:yukihiro_matsumoto][:languages] = []
 programmer_hash[:yukihiro_matsumoto][:languages] << "LISP"
-
+programmer_hash[:yukihiro_matsumoto][:languages] << "C"
 end
 
 def changing_alan
