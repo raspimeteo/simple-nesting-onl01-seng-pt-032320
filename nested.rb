b38a2b61c766e144b30cc2fc4039b5990239526d
John Zonneveld <jjczon@gmail.com>
2020-03-30 23:32:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,6 @@ def adding_matz
      }
 programmer_hash[:yukihiro_matsumoto]={}
 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-programmer_hash[:yukihiro_matsumoto][:languages] = []
 programmer_hash[:yukihiro_matsumoto][:languages] << "LISP"
 programmer_hash[:yukihiro_matsumoto][:languages] << "C"
 end
